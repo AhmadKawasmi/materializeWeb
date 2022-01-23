07cedcf25107f1085b3413561662e4adb08ea399
AhmadKawasmi <96664593+AhmadKawasmi@users.noreply.github.com>
2022-01-23 14:21:19 +0200

    saving some changes

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function() {
     $('.sidenav').sidenav();
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, {});
+  });
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
